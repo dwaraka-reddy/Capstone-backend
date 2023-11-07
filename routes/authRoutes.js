e315bce8048ca111ff8f20773cf78816290c6357
Dwaraka_Reddy <dwarakar42@gmail.com>
2023-11-07 17:58:04 +0530

    Updated SignUp route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, signIn, updateUser } = require('../controllers/authController');
 
-router.post('/signup', signUp);
+// Import the functions related to each step of the signup process
+const { 
+  signUpStep1, 
+  signUpStep2, 
+  signUpStep3, 
+  signIn, 
+  updateUser 
+} = require('../controllers/authController');
+
+// Existing routes
 router.post('/signin', signIn);
 router.put('/update-profile', updateUser);
+
+// New routes for the three-step signup process
+router.post('/signup/step1', signUpStep1);
+router.post('/signup/step2', signUpStep2);
+router.post('/signup/step3', signUpStep3);
 
 module.exports = router;
