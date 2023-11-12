59af89a7002a9cd9c8054404583bd8cb0ca156a8
Dwaraka_Reddy <dwarakar42@gmail.com>
2023-11-12 16:18:53 +0530

    Updated config file

--- config/database.js
+++ config/database.js
@@ -1,47 +1,30 @@
-// require('dotenv').config();
-// const { Sequelize } = require('sequelize');
-
-// let sequelize;
-
-// // Check if DATABASE_URL is set. If yes, use it.
-// if (process.env.DATABASE_URL) {
-//     sequelize = new Sequelize(process.env.DATABASE_URL, {
-//         dialect: 'postgres',
-//         protocol: 'postgres',
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false // This is to handle self-signed certificates.
-//             }
-//         }
-//     });
-// } else {
-//     sequelize = new Sequelize(
-//         process.env.DB_DATABASE,
-//         process.env.DB_USERNAME,
-//         process.env.DB_PASSWORD,
-//         {
-//             host: process.env.DB_HOST,
-//             dialect: 'postgres'
-//         }
-//     );
-// }
-
-// module.exports = sequelize;
-
-
-
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
-);
+let sequelize;
+
+// Check if DATABASE_URL is set. If yes, use it.
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // This is to handle self-signed certificates.
+            }
+        }
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_DATABASE,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres'
+        }
+    );
+}
 
 module.exports = sequelize;
